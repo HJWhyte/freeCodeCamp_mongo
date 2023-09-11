e637b00141721ea14463aa6bc86c083b7d537a1c
Harry Whyte~ <harrywhyte@kubrickgroup.com>
2023-09-11 14:32:59 +0100

    #4 Create many DB entries func added

--- myApp.js
+++ myApp.js
@@ -27,9 +27,21 @@ const createAndSavePerson = (done) => {
   })
 };
 
+
+// #4 
+
+var arrayOfPeople = [
+  {name: "Frankie", age: 74, favoriteFoods: ["Del Taco"]},
+  {name: "Sol", age: 76, favoriteFoods: ["roast chicken"]},
+  {name: "Robert", age: 78, favoriteFoods: ["wine"]}
+];
+
+
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
-};
+  Person.create(arrayOfPeople, function(err, people) {
+    if (err) return console.log(err);
+    done(null,people)}
+)};
 
 const findPeopleByName = (personName, done) => {
   done(null /*, data*/);
